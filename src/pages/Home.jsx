2ef2d07fab0a86b9a6ecbee945389e30d5b9501c
19130004 <19130004@st.hcmuaf.edu.vn>
2022-11-30 17:03:37 +0700

    add produc, product popular

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Helmet from "../components/Helmet";
 import HeroSlider from "../components/HeroSlider";
 import heroSliderData from "../assets/fake-data/hero-slider";
+import Section, { SectionBody, SectionTitle } from "../components/Section";
+import policy from "../assets/fake-data/policy";
+import PolicyCard from "../components/PolicyCard";
+import Grid from "../components/Grid";
+import productData from "../assets/fake-data/products";
+import ProductCard from "../components/ProductCard";
+import banner from "../assets/images/banner.png";
+import { Link } from "react-router-dom";
 const Home = () => {
   console.log(heroSliderData);
   return (
@@ -12,6 +20,61 @@ const Home = () => {
         auto={true}
         timeout={5000}
       />
+      <Section>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {policy.map((item, index) => (
+              <PolicyCard
+                key={index}
+                name={item.name}
+                description={item.description}
+                icon={item.icon}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionTitle> Top sản phẩm bán chạy</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(4).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionBody>
+          <Link to="/catalog">
+            <img src={banner} alt="" />
+          </Link>
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionTitle>phổ biến</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(12).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
     </Helmet>
   );
 };
